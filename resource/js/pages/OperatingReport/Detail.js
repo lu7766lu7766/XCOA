4e0813e23b891a7acccf64e60af9b5cfa1d0d7be
Jac Wang <lu7766lu7766@gmail.com>
2020-03-12 09:53:52 +0800

    bug fix (#4)

--- resource/js/pages/OperatingReport/Detail.js
+++ resource/js/pages/OperatingReport/Detail.js
@@ -1,27 +1,23 @@
 import ListMixins from './List'
 
 export default {
-  mixins: [ListMixins],
-  methods: {
-    async getDetail()
-    {
-      let body = _.pickBy({...this.$route.query})
-      if (this.$route.query.company_id)
-      {
-        body = Object.assign(body, {company_id: [this.$route.query.company_id]})
-      }
-      const res = await this.$thisApi.getDetail(body)
-      this.datas = res.data
-    },
-    getGroupByProcess()
-    {
-      return this.getGroupByNameDatas(this.datas, 'process_type', 'process_type')
-    },
-  },
-  computed: {
-    currentCompany()
-    {
-      return _.find(this.options.company, x => '' + x.id === this.$route.query.company_id) || {name: '总公司'}
-    },
-  },
+	mixins: [ListMixins],
+	methods: {
+		async getDetail() {
+			let body = _.pickBy({ ...this.$route.query })
+			if (this.$route.query.company_id) {
+				body = Object.assign(body, { company_id: [this.$route.query.company_id] })
+			}
+			const res = await this.$thisApi.getDetail(body)
+			this.datas = res.data
+		},
+		getGroupByProcess() {
+			return this.getGroupByNameDatas(this.datas, 'process_type', 'process_type')
+		},
+	},
+	computed: {
+		currentCompany() {
+			return _.find(this.options.company, x => x.id === +this.$route.query.company_id) || { name: '总公司' }
+		},
+	},
 }
